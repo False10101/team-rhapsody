a0b18179ae6912d567391aa3c311060f1ca8fdcf
Issac <minph7102004@gmail.com>
2025-10-27 15:30:00 +0700

    Feat: Data processing and cleaning (NaNs, outliers)

--- Final/model.py
+++ Final/model.py
@@ -28,3 +28,50 @@ print ("Data is loaded!")
 # Drop 'Id'
 train.drop(['Id'], axis=1, inplace=True)
 test.drop(['Id'], axis=1, inplace=True)
+
+# --- 3. DATA PROCESSING AND FEATURE ENGINEERING ---
+print("Starting data processing...")
+
+# Remove outliers
+train = train[train.GrLivArea < 4500]
+train.reset_index(drop=True, inplace=True)
+
+# Log transform target
+train["SalePrice"] = np.log1p(train["SalePrice"])
+y = train['SalePrice'].reset_index(drop=True)
+
+# Concat features
+train_features = train.drop(['SalePrice'], axis=1)
+test_features = test
+features = pd.concat([train_features, test_features]).reset_index(drop=True)
+
+# Convert numerical categories to strings
+features['MSSubClass'] = features['MSSubClass'].apply(str)
+features['YrSold'] = features['YrSold'].astype(str)
+features['MoSold'] = features['MoSold'].astype(str)
+
+# Fill NaNs
+features['Functional'] = features['Functional'].fillna('Typ')
+features['Electrical'] = features['Electrical'].fillna("SBrkr")
+features['KitchenQual'] = features['KitchenQual'].fillna("TA")
+features["PoolQC"] = features["PoolQC"].fillna("None")
+features['Exterior1st'] = features['Exterior1st'].fillna(features['Exterior1st'].mode()[0])
+features['Exterior2nd'] = features['Exterior2nd'].fillna(features['Exterior2nd'].mode()[0])
+features['SaleType'] = features['SaleType'].fillna(features['SaleType'].mode()[0])
+features['MasVnrType'] = features['MasVnrType'].fillna('None')
+
+for col in ('GarageYrBlt', 'GarageArea', 'GarageCars', 'MasVnrArea'):
+    features[col] = features[col].fillna(0)
+for col in ['GarageType', 'GarageFinish', 'GarageQual', 'GarageCond']:
+    features[col] = features[col].fillna('None')
+for col in ('BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2'):
+    features[col] = features[col].fillna('None')
+
+features['MSZoning'] = features.groupby('MSSubClass')['MSZoning'].transform(lambda x: x.fillna(x.mode()[0]))
+features['LotFrontage'] = features.groupby('Neighborhood')['LotFrontage'].transform(lambda x: x.fillna(x.median()))
+
+# Fill remaining NaNs
+objects = [i for i in features.columns if features[i].dtype == object]
+features.update(features[objects].fillna('None'))
+numerics = [i for i in features.columns if features[i].dtype != object]
+features.update(features[numerics].fillna(0))
